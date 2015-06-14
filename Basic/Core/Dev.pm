41288ce5383b5e72159146d0e28054ae3182a241
Ed J <mohawk2@users.noreply.github.com>
2015-06-14 21:55:03 +0100

    Add PDLA::Doc::add_module to pdlpp_postamble

--- Basic/Core/Dev.pm
+++ Basic/Core/Dev.pm
@@ -431,6 +431,10 @@ $pref.xs: $pref.pm
 $pref.c: $pref.xs
 
 $pref\$(OBJ_EXT): $pref.c
+
+install ::
+	\@echo "Updating PDL documentation database...";
+	\$(ABSPERLRUN) -e 'exit if \$\$ENV{DESTDIR}; use PDLA::Doc; eval { PDLA::Doc::add_module(q{$mod}); }; ';
 |
 	} (@_)
 }
