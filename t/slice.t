PDLPorters/pdl

Add tests for dice clump bug (sf #378)

t/slice.t
@@ -4,7 +4,7 @@
 use strict;
 use Test::More;
 
-plan tests => 89;
+plan tests => 92;
     ;
 use PDL::LiteF;
 
@@ -185,6 +185,13 @@ like($@, qr/slice ends out of bounds/);
 $a = sequence(10,4);
 is($a->dice([1,2],[0,3])->sum, 66, "dice");
 is($a->dice([0,1],'X')->sum, 124, "dice 'X'");
+
+# Test of dice clump compatability
+my $xxx = PDL->new([[[0,0]],[[1,1]],[[2,2]]]);
+is_deeply($xxx->where($xxx == 0)->unpdl,[0,0],"dice clump base zero");
+my $dice = $xxx->dice("X","X",[1,0]);
+is_deeply($dice->clump(-1)->unpdl,[1,1,0,0],"dice clump correct");
+is_deeply($dice->where($dice == 0)->unpdl,[0,0],"dice clump where zero");
 
 # Test of Reorder:
 $a = sequence(5,3,2);

PDLPorters/pdla-core

t/slice.t
@@ -4,7 +4,7 @@
 use strict;
 use Test::More;
 
-plan tests => 89;
+plan tests => 92;
     ;
 use PDLA::LiteF;
 
@@ -185,6 +185,13 @@ like($@, qr/slice ends out of bounds/);
 $a = sequence(10,4);
 is($a->dice([1,2],[0,3])->sum, 66, "dice");
 is($a->dice([0,1],'X')->sum, 124, "dice 'X'");
+
+# Test of dice clump compatability
+my $xxx = PDLA->new([[[0,0]],[[1,1]],[[2,2]]]);
+is_deeply($xxx->where($xxx == 0)->unpdl,[0,0],"dice clump base zero");
+my $dice = $xxx->dice("X","X",[1,0]);
+is_deeply($dice->clump(-1)->unpdl,[1,1,0,0],"dice clump correct");
+is_deeply($dice->where($dice == 0)->unpdl,[0,0],"dice clump where zero");
 
 # Test of Reorder:
 $a = sequence(5,3,2);