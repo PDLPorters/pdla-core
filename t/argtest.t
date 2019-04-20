PDLPorters/pdl

Add some test descriptions.

t/argtest.t
@@ -11,7 +11,7 @@ kill 'INT',$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
 {
 	my $pb=pdl([1,2,3])->long;
 	my $pa=[1,2,3];
-	lives_ok { PDL::Ufunc::sumover($pa,$pb) };
+	lives_ok { PDL::Ufunc::sumover($pa,$pb) } 'sumover with piddles of compatible dimensions does not die';
 }
 
 

t/bool.t
@@ -9,7 +9,7 @@ kill 'INT',$$  if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
 
 {
 	my $pa = zeroes 1,1,1;
-	ok !$pa;
+	ok !$pa, 'single-element multi-dim piddle collapses';
 }
 
 {
@@ -19,13 +19,13 @@ kill 'INT',$$  if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
 
 $a = ones 3;
 eval {print "oops\n" if $a};
-like $@, qr/multielement/;
+like $@, qr/multielement/, 'multielement piddle in conditional expression';
 
-ok all $a;
+ok all($a),'all elements true';
 
 $a = pdl byte, [ 0, 0, 1 ];
-ok any $a > 0;
+ok any($a > 0),'any element true';
 
 $a = ones 3;
 $b = $a + 1e-4;
-ok all PDL::approx $a, $b, 1e-3;
+ok all(PDL::approx $a, $b, 1e-3), 'approx';

t/clump.t
@@ -66,7 +66,7 @@ if(0) {
 
 	note $pb;
 
-	ok(all PDL::approx($pb,pdl([0,1,2,10,11,12,20,21,22]), $eps));
+	ok(all(PDL::approx($pb,pdl([0,1,2,10,11,12,20,21,22]), $eps)),'clump(-1) entire piddle');
 
 	# note $b;
 
@@ -84,9 +84,9 @@ if(0) {
 	# ok(2,$@ =~ /^clump: Increments do not match/);
 	# Clump supports this now.
 
-	ok(all PDL::approx($pd,pdl([0,2,10,12,20,22]), $eps));
+	ok(all(PDL::approx($pd,pdl([0,2,10,12,20,22]), $eps)),'clump(-1) slice with skip and whole dim');
 
-	ok(all PDL::approx($pe,pdl([10,12,20]), $eps));
+	ok(all(PDL::approx($pe,pdl([10,12,20]), $eps)),'clump(-1) slice');
 
         # SF bug #406 clump(-N) failure
         ##-- test data

PDLPorters/pdla-core

t/argtest.t
@@ -11,7 +11,7 @@ kill 'INT',$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
 {
 	my $pb=pdl([1,2,3])->long;
 	my $pa=[1,2,3];
-	lives_ok { PDLA::Ufunc::sumover($pa,$pb) };
+	lives_ok { PDLA::Ufunc::sumover($pa,$pb) } 'sumover with piddles of compatible dimensions does not die';
 }
 
 

t/bool.t
@@ -9,22 +9,23 @@ kill 'INT',$$  if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
 
 {
 	my $pa = zeroes 1,1,1;
-	ok !$pa;
+	ok !$pa, 'single-element multi-dim piddle collapses';
 }
 
 {
 	my $pa = ones 3;
-	throws_ok { print "oops\n" if $pa } qr/multielement/;
-	ok all $pa;
+	throws_ok { print "oops\n" if $pa } qr/multielement/,
+	  'multielement piddle in conditional expression';
+	ok all($pa),'all elements true';
 }
 
 {
 	my $pa = pdl byte, [ 0, 0, 1 ];
-	ok any $pa > 0;
+	ok any($pa > 0),'any element true';
 }
 
 {
 	my $pa = ones 3;
 	my $pb = $pa + 1e-4;
-	ok all PDLA::approx $pa, $pb, 1e-3;
+	ok all(PDLA::approx $pa, $pb, 1e-3), 'approx';
 }

t/clump.t
@@ -66,7 +66,7 @@ if(0) {
 
 	note $pb;
 
-	ok(all PDLA::approx($pb,pdl([0,1,2,10,11,12,20,21,22]), $eps));
+	ok(all(PDLA::approx($pb,pdl([0,1,2,10,11,12,20,21,22]), $eps)),'clump(-1) entire piddle');
 
 	# note $b;
 
@@ -84,9 +84,9 @@ if(0) {
 	# ok(2,$@ =~ /^clump: Increments do not match/);
 	# Clump supports this now.
 
-	ok(all PDLA::approx($pd,pdl([0,2,10,12,20,22]), $eps));
+	ok(all(PDLA::approx($pd,pdl([0,2,10,12,20,22]), $eps)),'clump(-1) slice with skip and whole dim');
 
-	ok(all PDLA::approx($pe,pdl([10,12,20]), $eps));
+	ok(all(PDLA::approx($pe,pdl([10,12,20]), $eps)),'clump(-1) slice');
 
         # SF bug #406 clump(-N) failure
         ##-- test data