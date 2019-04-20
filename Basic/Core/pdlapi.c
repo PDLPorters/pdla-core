PDLPorters/pdl

Add print of sizeof(pdl) when debugging is enabled
On my system, the pdl structure is 328 bytes (cygwin64/win7)

Basic/Core/pdlapi.c
@@ -144,7 +144,7 @@ pdl* pdl_create(int type) {
      it->magic = 0;
      it->hdrsv = 0;
 
-     PDLDEBUG_f(printf("CREATE %p\n",(void*)it));
+     PDLDEBUG_f(printf("CREATE %p (size=%d)\n",(void*)it,sizeof(pdl)));
      return it;
 }
 

PDLPorters/pdla-core

Basic/Core/pdlapi.c
@@ -144,7 +144,7 @@ pdl* pdl_create(int type) {
      it->magic = 0;
      it->hdrsv = 0;
 
-     PDLADEBUG_f(printf("CREATE %p\n",(void*)it));
+     PDLADEBUG_f(printf("CREATE %p (size=%d)\n",(void*)it,sizeof(pdl)));
      return it;
 }
 