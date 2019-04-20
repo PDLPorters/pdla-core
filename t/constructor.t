PDLPorters/pdl

fix skip count for no-badvals case

t/constructor.t
@@ -196,7 +196,7 @@ my $d = pdl(@c);
 ##############################
 # test bad values
  SKIP: {
-     skip "BAD values not compiled in",5 unless($PDL::Bad::Status);
+     skip "BAD values not compiled in",7 unless($PDL::Bad::Status);
      
      $a = pdl(3,4,5);
      $a=$a->setbadif($a==4);

PDLPorters/pdla-core

t/constructor.t
@@ -196,7 +196,7 @@ my $d = pdl(@c);
 ##############################
 # test bad values
  SKIP: {
-     skip "BAD values not compiled in",5 unless($PDLA::Bad::Status);
+     skip "BAD values not compiled in",7 unless($PDLA::Bad::Status);
      
      $a = pdl(3,4,5);
      $a=$a->setbadif($a==4);