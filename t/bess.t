PDLPorters/pdl

Convert a bunch more tests to Test::More.

t/bess.t
@@ -1,8 +1,6 @@
 # -*-perl-*-
 
-use Test;
-
-BEGIN { plan tests => 6; }
+use Test::More tests => 6;
 
 use PDL::LiteF;
 use PDL::Math;
@@ -16,17 +14,17 @@ sub tapprox {
         $d < 0.01;
 }
 
-ok( tapprox(bessj0(0.5),0.9384) && tapprox(bessj0(0),1) );
-ok( tapprox(bessj1(0.1),0.0499) && tapprox(bessj1(0),0) );
-ok( tapprox(bessjn(0.8,3),0.010) && tapprox(bessyn(0.2,2),-32.15714) );
+ok( tapprox(bessj0(0.5),0.9384) && tapprox(bessj0(0),1) ,"bessj0");
+ok( tapprox(bessj1(0.1),0.0499) && tapprox(bessj1(0),0) ,"bessj1");
+ok( tapprox(bessjn(0.8,3),0.010) && tapprox(bessyn(0.2,2),-32.15714) ,"bessjn");
 
 # test inplace
 $a = pdl(0.5,0.0);
 $a->inplace->bessj0;
-ok( tapprox($a,pdl(0.9384,1)) );
+ok( tapprox($a,pdl(0.9384,1)), "bessj0 inplace" );
 
 $a = pdl(0.2);
 $a->inplace->bessyn(2);
-ok( tapprox( $a, -32.15714 ) );   # 5
+ok( tapprox( $a, -32.15714 ) , "bessyn inplace");
 
-ok( tapprox( pow(2,3),8)); # test for the pow bug
+ok( tapprox( pow(2,3),8), "pow");

t/erf.t
@@ -1,30 +1,28 @@
 # -*-perl-*-
-
-use Test;
-
-BEGIN { plan tests => 5; }
-
+use strict;
+use warnings;
+use Test::More tests => 5;
 use PDL::LiteF;
 use PDL::Math;
 
-kill INT,$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
+kill 'INT',$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
 
 sub tapprox {
     my($a,$b) = @_;
-    $c = abs($a-$b);
-    $d = max($c);
+    my $c = abs($a-$b);
+    my $d = max($c);
     $d < 0.01;
 }
 
-ok( tapprox(erf(0.),0.) && tapprox(erf(30.),1.) );
-ok( tapprox(erf(0.5),1.-erfc(0.5)) );
-ok( tapprox(erf(erfi(0.5)),0.5) && tapprox(erfi(erf(0.5)),0.5) );
+ok( tapprox(erf(0.),0.) && tapprox(erf(30.),1.),"erf(0), erf(30)");
+ok( tapprox(erf(0.5),1.-erfc(0.5)), "erf and erfc");
+ok( tapprox(erf(erfi(0.5)),0.5) && tapprox(erfi(erf(0.5)),0.5), "erfi (both ways)");
 
 # now test inplace
 $a = pdl(0.0,30.0);
 $a->inplace->erf;
-ok( tapprox( $a, pdl(0.0,1.0) ) );
+ok( tapprox( $a, pdl(0.0,1.0) ), "erf inplace");
 
 $a = pdl(0.5);
-$a->inplace->erfc; 
-ok( tapprox( 1.0-$a, erf(0.5) ) );
+$a->inplace->erfc;
+ok( tapprox( 1.0-$a, erf(0.5) ), "erfc inplace");

t/lvalue.t
@@ -1,7 +1,8 @@
 use strict;
+use warnings;
 use English;
 
-use Test;
+use Test::More;
 
 use PDL::LiteF;
 use PDL::Lvalue;
@@ -18,11 +19,11 @@ BEGIN {
 
 $| = 1;
 
-ok (PDL::Lvalue->subs('slice'));
+ok (PDL::Lvalue->subs('slice'),"slice is an lvalue sub");
 
 $a = sequence 10;
 eval '$a->slice("") .= 0';
 
-ok (!$@);
+ok (!$@, "lvalue slice ran OK") or diag($@);
 
-ok ($a->max, 0);
+is ($a->max, 0, "lvalue slice modified values");

t/trig.t
@@ -1,26 +1,25 @@
 # -*-perl-*-
-
-use Test;
-
-BEGIN { plan tests => 4; }
+use strict;
+use warnings;
+use Test::More tests => 4;
 
 use PDL::LiteF;
 use PDL::Math;
 
-kill INT,$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
+kill 'INT',$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
 
 sub tapprox {
     my($a,$b) = @_;
-    $c = abs($a-$b);
-    $d = max($c);
+    my $c = abs($a-$b);
+    my $d = max($c);
     $d < 0.01;
 }
 
-ok( tapprox(sinh(0.3),0.3045) && tapprox(acosh(42.1),4.43305) );
-ok( tapprox(acos(0.3),1.2661) && tapprox(tanh(0.4),0.3799) );
-ok( tapprox(cosh(2.0),3.7621) && tapprox(atan(0.6),0.54041) );
+ok( tapprox(sinh(0.3),0.3045) && tapprox(acosh(42.1),4.43305), "sinh, acosh");
+ok( tapprox(acos(0.3),1.2661) && tapprox(tanh(0.4),0.3799), "acos, tanh");
+ok( tapprox(cosh(2.0),3.7621) && tapprox(atan(0.6),0.54041), "cosh, atan");
 
 # inplace
 $a = pdl(0.3);
 $a->inplace->sinh;
-ok( tapprox($a, pdl(0.3045)) );
+ok( tapprox($a, pdl(0.3045)), "sinh inplace");

t/xvals.t
@@ -1,72 +1,15 @@
-use Test;
-BEGIN { plan tests => 4; }
+use Test::More tests => 4;
 use PDL::LiteF;
-kill INT,$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
+use strict;
+use warnings;
+#If we've gotten this far and these tests fail, we're really in trouble!
 
+my $a0 = zeroes(3,2);
 
-$a0 = zeroes(3,2);
-# $a0->doflow();
+my $a1 = xvals $a0;
 
- print $a0;
-
-$a1 = $a0->slice('(1)');
-
- print $a1;
-
-# $a0->dump(); $a1->dump();
-
-# $a1->dump();
-
-$a1 += 4;
-
-# $a1->dump();
-
- print $a1;
-
-$dummy = PDL::Core::new_or_inplace($a0);
-print $dummy;
-$dummy2 = $dummy->xchg(0,0);
-print $dummy2;
-# $dummy2->dump();
-# $dummy->dump();
-PDL::Primitive::axisvalues($dummy2);
-# $dummy2->dump();
-# $dummy->dump();
-print $dummy2;
-print $dummy;
-
-
-
-# $a1->dump();
-
-# $a0->dump(); $a1->dump();
-
-# print $a1;
-
-# print $a0;
-
-# print $a1;
-
-$a = xvals $a0;
-
-print $a;
-
-ok($a->at(0,0) == 0);
-ok($a->at(1,0) == 1);
-ok($a->at(2,0) == 2);
-ok($a->at(1,1) == 1);
-
-$a = zeroes 5,10;
-
-$b = yvals $a;
-
-$c = $b->copy();
-
-$d = $b-$c;
-
-print "$d,$b,$c";
-
-# print $a;
-
-print "OUTOUT\n";
+is($a1->at(0,0), 0, "xvals 0,0 == 0");
+is($a1->at(1,0), 1, "xvals 1,0 == 1");
+is($a1->at(2,0), 2, "xvals 2,0 == 2");
+is($a1->at(1,1), 1, "xvals 1,1 == 1");
 

PDLPorters/pdla-core

t/bess.t
@@ -15,23 +15,23 @@ sub tapprox {
 	all approx $pa, $pb, 0.01;
 }
 
-ok( tapprox(bessj0(0.5),0.9384) && tapprox(bessj0(0),1) );
-ok( tapprox(bessj1(0.1),0.0499) && tapprox(bessj1(0),0) );
-ok( tapprox(bessjn(0.8,3),0.010) && tapprox(bessyn(0.2,2),-32.15714) );
+ok( tapprox(bessj0(0.5),0.9384) && tapprox(bessj0(0),1) ,"bessj0");
+ok( tapprox(bessj1(0.1),0.0499) && tapprox(bessj1(0),0) ,"bessj1");
+ok( tapprox(bessjn(0.8,3),0.010) && tapprox(bessyn(0.2,2),-32.15714) ,"bessjn");
 
 {
 # test inplace
 my $pa = pdl(0.5,0.0);
 $pa->inplace->bessj0;
-ok( tapprox($pa,pdl(0.9384,1)) );
+ok( tapprox($pa,pdl(0.9384,1)), "bessj0 inplace" );
 }
 
 {
 my $pa = pdl(0.2);
 $pa->inplace->bessyn(2);
-ok( tapprox( $pa, -32.15714 ) );   # 5
+ok( tapprox( $pa, -32.15714 ) , "bessyn inplace");
 }
 
 {
-ok( tapprox( pow(2,3),8)); # test for the pow bug
+ok( tapprox( pow(2,3),8), "pow");
 }

t/erf.t
@@ -14,19 +14,19 @@ sub tapprox {
     all approx $pa, $pb, 0.01;
 }
 
-ok( tapprox(erf(0.),0.) && tapprox(erf(30.),1.) );
-ok( tapprox(erf(0.5),1.-erfc(0.5)) );
-ok( tapprox(erf(erfi(0.5)),0.5) && tapprox(erfi(erf(0.5)),0.5) );
+ok( tapprox(erf(0.),0.) && tapprox(erf(30.),1.),"erf(0), erf(30)");
+ok( tapprox(erf(0.5),1.-erfc(0.5)), "erf and erfc");
+ok( tapprox(erf(erfi(0.5)),0.5) && tapprox(erfi(erf(0.5)),0.5), "erfi (both ways)");
 
 {
 # now test inplace
 my $pa = pdl(0.0,30.0);
 $pa->inplace->erf;
-ok( tapprox( $pa, pdl(0.0,1.0) ) );
+ok( tapprox( $pa, pdl(0.0,1.0) ), "erf inplace");
 }
 
 {
 my $pa = pdl(0.5);
 $pa->inplace->erfc;
-ok( tapprox( 1.0-$pa, erf(0.5) ) );
+ok( tapprox( 1.0-$pa, erf(0.5) ), "erfc inplace");
 }

t/lvalue.t
@@ -19,11 +19,11 @@ BEGIN {
 
 $| = 1;
 
-ok (PDLA::Lvalue->subs('slice'));
+ok (PDLA::Lvalue->subs('slice'),"slice is an lvalue sub");
 
 my $pa = sequence 10;
 lives_ok {
 	$pa->slice("") .= 0;
-};
+} "lvalue slice ran OK";
 
-is($pa->max, 0);
+is($pa->max, 0, "lvalue slice modified values");

t/trig.t
@@ -15,13 +15,13 @@ sub tapprox {
     all approx $pa, $pb, 0.01;
 }
 
-ok( tapprox(sinh(0.3),0.3045) && tapprox(acosh(42.1),4.43305) );
-ok( tapprox(acos(0.3),1.2661) && tapprox(tanh(0.4),0.3799) );
-ok( tapprox(cosh(2.0),3.7621) && tapprox(atan(0.6),0.54041) );
+ok( tapprox(sinh(0.3),0.3045) && tapprox(acosh(42.1),4.43305), "sinh, acosh");
+ok( tapprox(acos(0.3),1.2661) && tapprox(tanh(0.4),0.3799), "acos, tanh");
+ok( tapprox(cosh(2.0),3.7621) && tapprox(atan(0.6),0.54041), "cosh, atan");
 
 {
 # inplace
 my $pa = pdl(0.3);
 $pa->inplace->sinh;
-ok( tapprox($pa, pdl(0.3045)) );
+ok( tapprox($pa, pdl(0.3045)), "sinh inplace");
 }

t/xvals.t
@@ -53,22 +53,7 @@ my $pa = xvals $a0;
 
 note $pa;
 
-ok($pa->at(0,0) == 0);
-ok($pa->at(1,0) == 1);
-ok($pa->at(2,0) == 2);
-ok($pa->at(1,1) == 1);
-
-$pa = zeroes 5,10;
-
-my $pb = yvals $pa;
-
-my $c = $pb->copy();
-
-my $d = $pb-$c;
-
-note "$d,$pb,$c";
-
-# note $pa;
-
-note "OUTOUT\n";
-
+is($pa->at(0,0), 0, "xvals 0,0 == 0");
+is($pa->at(1,0), 1, "xvals 1,0 == 1");
+is($pa->at(2,0), 2, "xvals 2,0 == 2");
+is($pa->at(1,1), 1, "xvals 1,1 == 1");