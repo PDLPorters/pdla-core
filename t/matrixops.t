PDLPorters/pdl

Convert matrixops.t to use Test::More

t/matrixops.t
@@ -13,7 +13,7 @@ sub near {
 my $tol = 1e-14;
 
 eval 'use PDL::MatrixOps;';
-ok(!$@);
+ok(!$@, "use PDL::MatrixOps");
 
 
 ### Check LU decomposition of a simple matrix
@@ -21,49 +21,49 @@ ok(!$@);
 $a = pdl([1,2,3],[4,5,6],[7,1,1]);
 eval '($lu,$perm,$par) = lu_decomp($a)';
 
-ok(!$@);                                 # ran OK
-ok($par==-1);                            # parity is right
-ok(all($perm == pdl(2,1,0)));            # permutation is right
+ok(!$@, "lu_decomp 3x3 ran OK");
+ok($par==-1, "lu_decomp 3x3 correct parity");
+ok(all($perm == pdl(2,1,0)), "lu_decomp 3x3 correct permutation");
 
-$l = $lu->copy; 
+$l = $lu->copy;
 my $ldiag;
-($ldiag = $l->diagonal(0,1)) .= 1; 
+($ldiag = $l->diagonal(0,1)) .= 1;
 my $tmp;
 ($tmp = $l->slice("2,1"))   .= 0;
 ($tmp = $l->slice("1:2,0")) .= 0;
 
-$u = $lu->copy; 
+$u = $lu->copy;
 ($tmp = $u->slice("1,2"))   .= 0;
 ($tmp = $u->slice("0,1:2")) .= 0;
 
-ok(near($a,matmult($l,$u)->slice(":,-1:0"),$tol)); # LU = A (after depermutation)
+ok(near($a,matmult($l,$u)->slice(":,-1:0"),$tol),"LU = A (after depermutation)");
 
 ### Check LU decomposition of an OK singular matrix
 
 $b = pdl([1,2,3],[4,5,6],[7,8,9]);
 ($lu,$perm,$par) = lu_decomp($b);
 
-ok(defined $lu);
-ok($lu->flat->abs->at(-1) < $tol);
+ok(defined $lu, "lu_decomp singular matrix defined");
+ok($lu->flat->abs->at(-1) < $tol, "lu_decomp singular matrix small value");
 
 ### Check inversion -- this also checks lu_backsub
 
 $a1 = inv($a,$opt={s=>1,lu=>\@a});
 $identity = zeroes(3,3); ($tmp = $identity->diagonal(0,1))++;
 
-ok(defined $a1);
-ok(ref ($opt->{lu}->[0]) eq 'PDL');
-ok(near(matmult($a1,$a),$identity,$tol));
+ok(defined $a1, "3x3 inverse: defined");
+ok(ref ($opt->{lu}->[0]) eq 'PDL',"inverse: lu_decomp first entry is a piddle");
+ok(near(matmult($a1,$a),$identity,$tol),"matrix mult by its inverse gives identity matrix");
 
 ### Check inv() with added thread dims (simple check)
 my $C22 = pdl([5,5],[5,7.5]);
 my $C22inv = eval { $C22->inv };
-ok(!$@);                                                    # ran OK
-ok(near($C22inv,pdl([0.6, -0.4], [-0.4, 0.4])));            # right answer
+ok(!$@, "2x2 inv ran OK");
+ok(near($C22inv,pdl([0.6, -0.4], [-0.4, 0.4])), "2x2 inv gave correct answer");
 my $C222 = $C22->dummy(2,2);
 my $C222inv = eval { $C222->inv };
-ok(!$@);                                                         # ran OK
-ok(near($C222inv,pdl([0.6, -0.4], [-0.4, 0.4])->dummy(2,2)));    # right answer
+ok(!$@, "2x2 w/ dummy dims ran OK");
+ok(near($C222inv,pdl([0.6, -0.4], [-0.4, 0.4])->dummy(2,2)), "2x2 w/ dummy dims correct answer");
 
 ### Check inv() for matrices with added thread dims (bug #3172882 on sf.net)
 $a94 = pdl( [  1,  0,  4, -1, -1, -3,  0,  1,  0 ],
@@ -74,8 +74,8 @@ $a94 = pdl( [  1,  0,  4, -1, -1, -3,  0,  1,  0 ],
 $a334 = $a94->reshape(3,3,4);
 eval '$a334inv = $a334->inv';
 
-ok(!$@);                                                    # ran OK
-ok(near(matmult($a334,$a334inv),$identity->dummy(2,4)));     # right answer
+ok(!$@, "3x3x4 inv ran OK");
+ok(near(matmult($a334,$a334inv),$identity->dummy(2,4)),"3x3x4 inv gave correct answer");
 
 undef $a94;       # clean up variables
 undef $a334;      # clean up variables
@@ -89,24 +89,24 @@ undef $a334inv;   # clean up variables
 $a = pdl([[2,1],[1,2]]);
 eval '($lu,$perm,$par) = lu_decomp($a)';
 
-ok(!$@);                                 # ran OK
-ok($par==1);                             # parity is right
-ok(all($perm == pdl(0,1)));              # permutation is right
+ok(!$@, "lu_decomp 2x2 ran OK");
+ok($par==1, "lu_decomp 2x2 correct parity");
+ok(all($perm == pdl(0,1)), "lu_decomp 2x2 correct permutation");
 
 $bb = pdl([1,0]);
 eval '$xx = lu_backsub($lu,$perm,$bb)';
-ok(!$@);                                 # ran OK
+ok(!$@, "lu_backsub ran OK");
 my $xx_shape = pdl($xx->dims);
 my $bb_shape = pdl($bb->dims);
-ok(all($xx_shape == $bb_shape));        # check that soln and input have same shape
-ok(near($xx,pdl([2/3, -1/3]),$tol));     # LU = A (after depermutation)
+ok(all($xx_shape == $bb_shape), "lu_backsub solution and input have same shape");
+ok(near($xx,pdl([2/3, -1/3]),$tol), "lu_backsub LU=A (after depermutation)");
 
 
 ### Check attempted inversion of a singular matrix
 $b2=undef; # avoid warning from compiler
 eval '$b2 = inv($b,{s=>1})';
-ok(!$@);
-ok(!defined $b2);
+ok(!$@, "inv of singular matrix should not barf if s=>1");
+ok(!defined $b2, "inv of singular matrix undefined if s=>1");
 
 
 ### Check threaded determinant -- simultaneous recursive det of four 4x4's
@@ -116,30 +116,30 @@ $c = pdl([0,1,0,0],[1,0,0,0],[0,0,1,0],[0,0,0,1]); # det=-1
 $d = pdl([1,2,3,4],[5,4,3,2],[0,0,3,0],[3,0,1,6]); # det=-216
 $e = ($a->cat($b)) -> cat( $c->cat($d) );
 $det = $e->determinant;
-ok(all($det == pdl([48,1],[-1,-216])));
+ok(all($det == pdl([48,1],[-1,-216])),"threaded determinant");
 
 ### Check identity and stretcher matrices...
-ok((identity(2)->flat == pdl(1,0,0,1))->all);
+ok((identity(2)->flat == pdl(1,0,0,1))->all, "identity matrix");
 
-ok((stretcher(pdl(2,3))->flat == pdl(2,0,0,3))->all);
+ok((stretcher(pdl(2,3))->flat == pdl(2,0,0,3))->all, "stretcher 2x2");
 
-ok((stretcher(pdl([2,3],[3,4]))->flat == pdl(2,0,0,3,3,0,0,4))->all);
+ok((stretcher(pdl([2,3],[3,4]))->flat == pdl(2,0,0,3,3,0,0,4))->all,"stretcher 2x2x2");
 
 ### Check eigens
 $a = pdl([3,4],[4,-3]);
 
 ### Check that eigens runs OK
 eval {($vec,$val) = eigens $a};
-ok(!$@);
+ok(!$@,"eigens runs OK");
 
 ### Check that it really returns eigenvectors
-$c = float(($a x $vec) / $vec); 
+$c = float(($a x $vec) / $vec);
 #print "c is $c\n";
-ok(all($c->slice(":,0") == $c->slice(":,1")));
+ok(all($c->slice(":,0") == $c->slice(":,1")),"eigens really returns eigenvectors");
 
 ### Check that the eigenvalues are correct for this matrix
-ok((float($val->slice("0")) == - float($val->slice("1")) and 
-	float($val->slice("0") * $val->slice("1")) == float(-25)));
+ok((float($val->slice("0")) == - float($val->slice("1")) and
+	float($val->slice("0") * $val->slice("1")) == float(-25)),"eigenvalues are correct");
 
 ### Check computations on larger matrix with known eigenvalue sum.
 my $m = pdl(
@@ -158,7 +158,7 @@ eval {
     $esum=sprintf "%.3f", sum($val); #signature of eigenvalues
 };
 #print STDERR "eigensum for the 8x8: $esum\n";
-ok($esum == 61.308);
+ok($esum == 61.308,"eigens sum for 8x8 correct answer");
 
 if(0){ #fails because of bad eigenvectors
 #Check an assymmetric matrix:
@@ -175,8 +175,8 @@ $esum=0;
 eval {
     $esum = sprintf "%.3f", sum scalar eigens_sym($m);
 };
-ok(!$@);
-ok($esum == 61.308);
+ok(!$@, "eigens_sym for 8x8 ran OK");
+ok($esum == 61.308, "eigens_sym sum for 8x8 correct answer");
 
 if(0){ #eigens for asymmetric matrices disbled
 #The below matrix has complex eigenvalues

PDLPorters/pdla-core

t/matrixops.t
@@ -22,13 +22,13 @@ use PDLA::MatrixOps;
 
 my $pa = pdl([1,2,3],[4,5,6],[7,1,1]);
 my ($lu, $perm, $par);
-lives_ok { ($lu,$perm,$par) = lu_decomp($pa); }; # ran OK
-is($par, -1);			    # parity is right
-ok(all($perm == pdl(2,1,0)));	    # permutation is right
+lives_ok { ($lu,$perm,$par) = lu_decomp($pa); } "lu_decomp 3x3 ran OK"; # ran OK
+is($par, -1, "lu_decomp 3x3 correct parity");
+ok(all($perm == pdl(2,1,0)), "lu_decomp 3x3 correct permutation");
 
 my $l = $lu->copy;
 my $ldiag;
-($ldiag = $l->diagonal(0,1)) .= 1; 
+($ldiag = $l->diagonal(0,1)) .= 1;
 my $tmp;
 ($tmp = $l->slice("2,1"))   .= 0;
 ($tmp = $l->slice("1:2,0")) .= 0;
@@ -37,7 +37,7 @@ my $u = $lu->copy;
 ($tmp = $u->slice("1,2"))   .= 0;
 ($tmp = $u->slice("0,1:2")) .= 0;
 
-ok(tapprox($pa,matmult($l,$u)->slice(":,-1:0"),$tol)); # LU = A (after depermutation)
+ok(tapprox($pa,matmult($l,$u)->slice(":,-1:0"),$tol), " LU = A (after depermutation)");
 }
 
 {
@@ -46,8 +46,8 @@ ok(tapprox($pa,matmult($l,$u)->slice(":,-1:0"),$tol)); # LU = A (after depermuta
 my $pb = pdl([1,2,3],[4,5,6],[7,8,9]);
 my ($lu,$perm,$par) = lu_decomp($pb);
 
-ok(defined $lu);
-ok($lu->flat->abs->at(-1) < $tol);
+ok(defined $lu, "lu_decomp singular matrix defined");
+ok($lu->flat->abs->at(-1) < $tol, "lu_decomp singular matrix small value");
 }
 
 {
@@ -58,21 +58,22 @@ my $opt ={s=>1,lu=>\my @a};
 my $a1 = inv($pa, $opt);
 my $identity = zeroes(3,3); (my $tmp = $identity->diagonal(0,1))++;
 
-ok(defined $a1);
-ok(ref ($opt->{lu}->[0]) eq 'PDLA');
-ok(tapprox(matmult($a1,$pa),$identity,$tol));
+ok(defined $a1, "3x3 inverse: defined");
+ok(ref ($opt->{lu}->[0]) eq 'PDLA',"inverse: lu_decomp first entry is a piddle");
+ok(tapprox(matmult($a1,$pa),$identity,$tol),"matrix mult by its inverse gives identity matrix")
+  or diag explain [ $a1.'', $pa.'' ];
 }
 
 {
 ### Check inv() with added thread dims (simple check)
 my $C22 = pdl([5,5],[5,7.5]);
 my $C22inv;
-lives_ok { $C22inv = $C22->inv };                           # ran OK
-ok(tapprox($C22inv,pdl([0.6, -0.4], [-0.4, 0.4])));	    # right answer
+lives_ok { $C22inv = $C22->inv } "2x2 inv ran OK";
+ok(tapprox($C22inv,pdl([0.6, -0.4], [-0.4, 0.4])), "2x2 inv gave correct answer");
 my $C222 = $C22->dummy(2,2);
 my $C222inv;
-lives_ok { $C222inv = $C222->inv };                              # ran OK
-ok(tapprox($C222inv,pdl([0.6, -0.4], [-0.4, 0.4])->dummy(2,2)));    # right answer
+lives_ok { $C222inv = $C222->inv } "2x2 w/ dummy dims ran OK";
+ok(tapprox($C222inv,pdl([0.6, -0.4], [-0.4, 0.4])->dummy(2,2)), "2x2 w/ dummy dims correct answer");
 }
 
 {
@@ -84,9 +85,9 @@ my $a94 = pdl( [  1,  0,  4, -1, -1, -3,  0,  1,  0 ],
 	     );
 my $a334 = $a94->reshape(3,3,4);
 my $a334inv;
-lives_ok { $a334inv = $a334->inv };                          # ran OK
+lives_ok { $a334inv = $a334->inv } "3x3x4 inv ran OK";
 my $identity = zeroes(3,3); (my $tmp = $identity->diagonal(0,1))++;
-ok(tapprox(matmult($a334,$a334inv),$identity->dummy(2,4)));     # right answer
+ok(tapprox(matmult($a334,$a334inv),$identity->dummy(2,4)),"3x3x4 inv gave correct answer");
 
 undef $a94;       # clean up variables
 undef $a334;      # clean up variables
@@ -99,17 +100,17 @@ undef $a334inv;   # clean up variables
 
 my $pa = pdl([[2,1],[1,2]]);
 my ($lu,$perm,$par);
-lives_ok { ($lu,$perm,$par) = lu_decomp($pa) }; # ran OK
-ok($par==1);			     # parity is right
-ok(all($perm == pdl(0,1)));	      # permutation is right
+lives_ok { ($lu,$perm,$par) = lu_decomp($pa) } "lu_decomp 2x2 ran OK";
+ok($par==1, "lu_decomp 2x2 correct parity");
+ok(all($perm == pdl(0,1)), "lu_decomp 2x2 correct permutation");
 
 my $bb = pdl([1,0]);
 my $xx;
-lives_ok { $xx = lu_backsub($lu,$perm,$bb) }; # ran OK
+lives_ok { $xx = lu_backsub($lu,$perm,$bb) } "lu_backsub ran OK";
 my $xx_shape = pdl($xx->dims);
 my $bb_shape = pdl($bb->dims);
-ok(all($xx_shape == $bb_shape));	# check that soln and input have same shape
-ok(tapprox($xx,pdl([2/3, -1/3]),$tol));     # LU = A (after depermutation)
+ok(all($xx_shape == $bb_shape), "lu_backsub solution and input have same shape");
+ok(tapprox($xx,pdl([2/3, -1/3]),$tol), "lu_backsub LU=A (after depermutation)");
 }
 
 {
@@ -129,16 +130,16 @@ my $c = pdl([0,1,0,0],[1,0,0,0],[0,0,1,0],[0,0,0,1]); # det=-1
 my $d = pdl([1,2,3,4],[5,4,3,2],[0,0,3,0],[3,0,1,6]); # det=-216
 my $e = ($pa->cat($pb)) -> cat( $c->cat($d) );
 my $det = $e->determinant;
-ok(all($det == pdl([48,1],[-1,-216])));
+ok(all($det == pdl([48,1],[-1,-216])),"threaded determinant");
 }
 
 {
 ### Check identity and stretcher matrices...
-ok((identity(2)->flat == pdl(1,0,0,1))->all);
+ok((identity(2)->flat == pdl(1,0,0,1))->all, "identity matrix");
 
-ok((stretcher(pdl(2,3))->flat == pdl(2,0,0,3))->all);
+ok((stretcher(pdl(2,3))->flat == pdl(2,0,0,3))->all, "stretcher 2x2");
 
-ok((stretcher(pdl([2,3],[3,4]))->flat == pdl(2,0,0,3,3,0,0,4))->all);
+ok((stretcher(pdl([2,3],[3,4]))->flat == pdl(2,0,0,3,3,0,0,4))->all,"stretcher 2x2x2");
 }
 
 {
@@ -147,16 +148,16 @@ my $pa = pdl([3,4],[4,-3]);
 
 ### Check that eigens runs OK
 my ($vec,$val);
-lives_ok { ($vec,$val) = eigens $pa };
+lives_ok { ($vec,$val) = eigens $pa } "eigens runs OK";
 
 ### Check that it really returns eigenvectors
 my $c = float(($pa x $vec) / $vec);
 #print "c is $c\n";
-ok(all($c->slice(":,0") == $c->slice(":,1")));
+ok(all($c->slice(":,0") == $c->slice(":,1")),"eigens really returns eigenvectors");
 
 ### Check that the eigenvalues are correct for this matrix
-ok((float($val->slice("0")) == - float($val->slice("1")) and 
-	float($val->slice("0") * $val->slice("1")) == float(-25)));
+ok((float($val->slice("0")) == - float($val->slice("1")) and
+       float($val->slice("0") * $val->slice("1")) == float(-25)),"eigenvalues are correct");
 }
 
 {
@@ -179,15 +180,15 @@ eval {
     $esum=sprintf "%.3f", sum($val); #signature of eigenvalues
 };
 #print STDERR "eigensum for the 8x8: $esum\n";
-ok($esum == 61.308);
+ok($esum == 61.308,"eigens sum for 8x8 correct answer");
 }
 
 {
 my $esum=0;
 lives_ok {
     $esum = sprintf "%.3f", sum scalar eigens_sym($m);
-};
-ok($esum == 61.308);
+} "eigens_sym for 8x8 ran OK";
+ok($esum == 61.308, "eigens_sym sum for 8x8 correct answer");
 }
 
 }