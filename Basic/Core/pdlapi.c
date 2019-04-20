PDLPorters/pdl

Change a printf conversion flag for sizeof().
Using %d was causing a compiler warning, %zu should be better.

Basic/Core/pdlapi.c
@@ -144,7 +144,7 @@ pdl* pdl_create(int type) {
      it->magic = 0;
      it->hdrsv = 0;
 
-     PDLDEBUG_f(printf("CREATE %p (size=%d)\n",(void*)it,sizeof(pdl)));
+     PDLDEBUG_f(printf("CREATE %p (size=%zu)\n",(void*)it,sizeof(pdl)));
      return it;
 }
 

PDLPorters/pdla-core

Basic/Core/pdlapi.c
@@ -144,7 +144,7 @@ pdl* pdl_create(int type) {
      it->magic = 0;
      it->hdrsv = 0;
 
-     PDLADEBUG_f(printf("CREATE %p (size=%d)\n",(void*)it,sizeof(pdl)));
+     PDLADEBUG_f(printf("CREATE %p (size=%zu)\n",(void*)it,sizeof(pdl)));
      return it;
 }
 