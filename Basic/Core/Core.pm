PDLPorters/pdl

Small POD fix for nslice.

Basic/Core/Core.pm
@@ -2188,7 +2188,7 @@ sub PDL::nslice_if_pdl {
 
 =for ref
 
-c<nslice> was an internally used interface for L<PDL::NiceSlice|PDL::NiceSlice>,
+C<nslice> was an internally used interface for L<PDL::NiceSlice|PDL::NiceSlice>,
 but is now merely a springboard to L<PDL::Slice|PDL::Slice>.  It is deprecated
 and likely to disappear in PDL 3.0.
 

PDLPorters/pdla-core

Basic/Core/Core.pm
@@ -2187,7 +2187,7 @@ sub PDLA::nslice_if_pdl {
 
 =for ref
 
-c<nslice> was an internally used interface for L<PDLA::NiceSlice|PDLA::NiceSlice>,
+C<nslice> was an internally used interface for L<PDLA::NiceSlice|PDLA::NiceSlice>,
 but is now merely a springboard to L<PDLA::Slice|PDLA::Slice>.  It is deprecated
 and likely to disappear in PDLA 3.0.
 